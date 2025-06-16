415bbe803a47ffdeebd3ed7ee3566305dbdeeb76
vtiquet <valentin.tiquet@gmail.com>
2025-06-16 10:17:09 +0000

    Hello, puts.4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+* This is my first C program
+* it prints the message: "Programming is like building a multilingual puzzle
+* main - entry point
+* return: always 0 (succes)
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
