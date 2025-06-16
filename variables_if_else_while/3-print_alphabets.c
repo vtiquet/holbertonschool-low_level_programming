34a69ba9e88a744a5adcb671d47527baa52d4b72
vtiquet <valentin.tiquet@gmail.com>
2025-06-16 17:38:49 +0000

    alphABET

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,24 +1,15 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-
 /**
 * main - Entry point
 * Return: Always 0 (Success)
 */
-
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-if (n > 0)
-printf("%d is positive\n", n);
-else if (n == 0)
-printf("%d is zero\n", n);
-else
-printf("%d is negative\n", n);
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+for (letter = 'A'; letter <= 'Z'; letter++)
+putchar(letter);
+putchar('\n');
 return (0);
 }
