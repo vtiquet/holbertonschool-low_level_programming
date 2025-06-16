b0921556f249ee5170269f247cc29590f9368bdd
vtiquet <valentin.tiquet@gmail.com>
2025-06-16 19:18:17 +0000

    Patience, persistence and perspiration make an unbeatable combination for success

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,24 +1,19 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-
 /**
 * main - Entry point
 * Return: Always 0 (Success)
 */
-
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-if (n > 0)
-printf("%d is positive\n", n);
-else if (n == 0)
-printf("%d is zero\n", n);
-else
-printf("%d is negative\n", n);
+int num;
+for (num = 0; num <= 9; num++)
+{
+putchar((num % 10) + '0');
+if (num == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
 return (0);
 }
