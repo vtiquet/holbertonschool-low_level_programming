f05770251c3dbff5496bbfad6604a9416140ea9e
vtiquet <valentin.tiquet@gmail.com>
2025-06-17 11:40:35 +0000

    Created 5-sign.c & 5-sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* _print sign - prints the sign of a number.
+* @n: represent a unsigned number.
+* Return: 1 if the number is greater than zero, 0 if the number is zero, -1 otherwise.
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
+}
