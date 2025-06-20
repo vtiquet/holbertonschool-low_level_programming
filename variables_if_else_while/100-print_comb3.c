cc3bd8f87d7b1d1fb92eeaa4179ed087b005b164
vtiquet <valentin.tiquet@gmail.com>
2025-06-20 11:01:12 +0000

    Fixed 100-print_comb3.c

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
-/**
-* main - Entry point
-* Return: Always 0 (Success)
-*/
+/*
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int n1, n2;
 for (n1 = 0; n1 < 9; n1++)
 {
-for (n2 = n1 + 1; n2 <=9; n2++)
+for (n2 = n1 + 1; n2 <= 9; n2++)
 {
 putchar ((n1 % 10) + '0');
 putchar ((n2 % 10) + '0');
-if (n1 < 8)
+if (n1 == 8 && n2 == 9)
+continue;
 putchar(',');
 putchar(' ');
 }
