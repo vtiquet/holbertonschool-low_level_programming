950850b2a7ca5056f2e663b20a54c66149e1e15a
vtiquet <valentin.tiquet@gmail.com>
2025-06-20 10:45:56 +0000

    Created 100-print_comb3.c

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n1, n2;
+for (n1 = 0; n1 < 9; n1++)
+{
+for (n2 = n1 + 1; n2 <=9; n2++)
+{
+putchar ((n1 % 10) + '0');
+putchar ((n2 % 10) + '0');
+if (n1 < 9)
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
