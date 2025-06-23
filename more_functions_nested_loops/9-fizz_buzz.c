14789165bd277d992f6892f87920d66aa5d6a174
vtiquet <valentin.tiquet@gmail.com>
2025-06-23 12:16:13 +0000

    Created 9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n;
+for (n = 1; n <= 100; n++)
+{
+if (n % 3 == 0 && n % 3 != 0)
+{
+printf("Fizz");
+}
+else if (n % 5 == 0 && n % 5 != 0)
+{
+printf("Buzz");
+}
+else if (n % 3 == 0 && n % 5 == 0)
+{
+printf("FizzBuzz");
+}
+else
+{
+printf("%d, ", n);
+printf("%d\n", n);
+}
+return (0);
+}
+}
