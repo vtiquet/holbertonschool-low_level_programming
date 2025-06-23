3a148e1432ce0145bb278785572a0917aaec165a
vtiquet <valentin.tiquet@gmail.com>
2025-06-23 07:50:38 +0000

    Created more_functions_nested_loops dir; maih.h header; 0-isupper.c program; REAME.md

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _islower - check if the character is lowercase
+* @c: represent a unsigned charactere
+* Return: 1 if character is lowercase, 0 otherwise.
+*/
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
