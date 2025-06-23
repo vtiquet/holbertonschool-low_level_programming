cbc0bb221209e3a48d43500ad8e69670a967323e
vtiquet <valentin.tiquet@gmail.com>
2025-06-23 08:31:32 +0000

    Created 5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* more_numbers - prints 10 times the numbers.
+*/
+void more_numbers(void)
+{
+int i = 0, n;
+while (i < 10)
+{
+for (n = 0; n <= 14; n++)
+_putchar(n);
+_putchar('\n');
+i++;
+}
+}
