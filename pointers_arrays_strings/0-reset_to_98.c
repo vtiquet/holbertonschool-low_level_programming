dc7fe1950e0fb5320b05f47ae55df02fe423ed72
vtiquet <valentin.tiquet@gmail.com>
2025-06-24 08:19:46 +0000

    Fixed 0-reset_to_98.c to comply with Betty coding style

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
-* reset_to_98 - a function that takes a pointer to an int as parameter and updates the value it points to to 98.
+* reset_to_98 - a function that takes a pointer to an int as parameter
+* and updates the value it points to to 98.
 * @n: an integer
 */
 void reset_to_98(int *n)
 {
-*n=98;
+*n = 98;
 }
