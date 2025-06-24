301e8a27fc000b32369e06b0617fa4060d9f1998
vtiquet <valentin.tiquet@gmail.com>
2025-06-24 13:29:07 +0000

    Created 6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* puts2 - function that prints every other character of a string.
+* @str: The string to be printed.
+*/
+void puts2(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*str);
+str += 2;
+}
+_putchar('\n');
+}
