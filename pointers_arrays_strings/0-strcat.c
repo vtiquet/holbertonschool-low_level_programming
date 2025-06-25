00ee7a4e6db429a4454299ee958eb1961656fef6
vtiquet <valentin.tiquet@gmail.com>
2025-06-25 12:49:44 +0000

    Added prototypes in main.h

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* _atoi - function that convert a string to an integer.
+* @s: the string.
+* Return: The integer value of the string, or 0 if no numbers are present
+*/
+int _atoi(char *s)
+{
+int i = 0, sign = 1;
+unsigned int n = 0;
+while (s[i] != '\0')
+{
+if (s[i] == '-')
+{
+sign *= -1;
+}
+else if (s[i] >= '0' && s[i] <= '9')
+{
+n = (n * 10) + (s[i] - '0');
+}
+else if (n > 0)
+{
+break;
+}
+i++;
+}
+return (sign *n);
+}
