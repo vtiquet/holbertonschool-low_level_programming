5794d48a359e8d2baf982b585319c9aa95178004
vtiquet <valentin.tiquet@gmail.com>
2025-06-25 08:45:22 +0000

    Created 9-strcpy.c

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _strcpy - function that copies the string to the buffer.
+* @dest: the buffer.
+* @src: the string.
+* Return: the pointer to dest.
+*/
+char *_strcpy(char *dest, char *src)
+{
+int  i = 0;
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+i++;
+dest[i] = src[i];
+return (dest);
+}
