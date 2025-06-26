39f1d2692712e5e921d3b7cece61000f1da05681
vtiquet <valentin.tiquet@gmail.com>
2025-06-26 11:58:01 +0000

    Created 2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _strncpy - function that copies the string to the buffer for n bytes.
+* @dest: the buffer.
+* @src: the string.
+* @n: the number of bytes to copy.
+* Return: the pointer to dest.
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+int  i = 0;
+while (src[i] != '\0' && i < n)
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+return (dest);
+}
