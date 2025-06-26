29b900fb6bd0f87141d5f849493f56c3c08cfb5a
vtiquet <valentin.tiquet@gmail.com>
2025-06-26 13:48:01 +0000

    Fixed 3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -8,12 +8,9 @@
 int _strcmp(char *s1, char *s2)
 {
 int i = 0, l = 0;
-while (s1[i] != '\0')
+while (s1[i] != '\0' && s2[l] != '\0' && s1[i] == s2[l])
 {
 i++;
-}
-while (s2[l] != '\0')
-{
 l++;
 }
 return (*s1 - *s2);
