c2d1027e8b7be94af0ff54efb4173a4053f141ac
vtiquet <valentin.tiquet@gmail.com>
2025-06-27 10:04:02 +0000

    Fixed 5-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -9,11 +9,10 @@ char *cap_string(char *s)
 int i = 0;
 while (s[i] != '\0')
 {
+while (!(s[i] >= 'a' && s[i] <= 'z'))
+{
 i++;
 }
-{
-while (!(s[i] >= 'a' && s[i] <= 'z'))
-i++;
 if (s[i - 1] == ' ' ||
 s[i - 1] == '\t' ||
 s[i - 1] == '\n' ||
@@ -28,7 +27,9 @@ s[i - 1] == ')' ||
 s[i - 1] == '{' ||
 s[i - 1] == '}' ||
 i == 0)
+{
 s[i] = s[i] - 32;
+}
 i++;
 }
 return (s);
