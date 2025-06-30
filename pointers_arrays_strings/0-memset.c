72b1f2115d0876577b60d634107e82b2148b8a1a
vtiquet <valentin.tiquet@gmail.com>
2025-06-30 07:18:08 +0000

    Created 0-memset.c

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _memset -  function that fills memory with a constant byte.
+* @s: memory area.
+* @b: constant byte.
+* @n: the number of bytes.
+* Return: the memory area.
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+ 
+    return (s);
+}
