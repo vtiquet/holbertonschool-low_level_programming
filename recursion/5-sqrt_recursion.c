e7eedb56e0b404ade315927c74b6ce321dd3dd00
vtiquet <valentin.tiquet@gmail.com>
2025-07-01 13:07:42 +0000

    Updated files

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,5 +1,26 @@
 #include "main.h"
 /**
+ * _find_sqrt - a function to recursively find the natural square root.
+ * @n: The number for which to find the square root.
+ * @y: The current guess for the square root.
+ * Return: The natural square root of @n,
+ * or -1 if no natural square root exists.
+ */
+int _find_sqrt(int n, int y)
+{
+
+	if (y * y == n)
+	{
+		return (y);
+	}
+	if (y * y > n)
+	{
+		return (-1);
+	}
+	return (_find_sqrt(n, y + 1));
+}
+
+/**
 * _sqrt_recursion -  a function that returns
 * the natural square root of a number.
 * @n: a given number.
@@ -12,5 +33,5 @@ int _sqrt_recursion(int n)
 	else if (n == 0)
 		return (0);
 	else
-		return (n * _sqrt_recursion(n, 1));
+		return (_find_sqrt(n, 1));
 }
--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,17 +1,36 @@
 #include "main.h"
 /**
-* _pow_recursion -  a function that returns the value of x
-* raised to the power of y.
-* @x: a given number.
-* @y: a given power.
-* Return: the value of x by the power of y.
-*/
-int _pow_recursion(int x, int y)
+ * check_prime - Helper function to recursively check for primality.
+ * @n: the number.
+ * @y: the denominator.
+ * Return: 1 if n is prime, 0 otherwise.
+ */
+int check_prime(int n, int y)
 {
-	if (y < 0)
-		return (-1);
-	else if (y == 0)
+    if (y * y > n)
+    {
+        return (1);
+    }
+    if (n % y == 0)
+    {
+        return (0);
+    }
+
+    return (check_prime(n, y + 2));
+}
+/**
+* is_prime_number -  a function that returns 1 if the input
+* integer is a prime number, otherwise return 0.
+* @n: a given number.
+* Return: 1 if the input integer is a prime number, otherwise return 0.
+*/
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (0);
+	else if (n == 2)
 		return (1);
-	else
-		return (x * _pow_recursion(x, y - 1));
+	else if (n % 2 == 0)
+		return (0);
+	return (check_prime(n, 3));
 }
