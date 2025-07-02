79bcc54ba130c2c2b5d85523190633c4043b28a3
vtiquet <valentin.tiquet@gmail.com>
2025-07-02 09:36:41 +0000

    updated 100-times_table.c

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -5,22 +5,38 @@
 */
 void print_times_table(int n)
 {
-int i, x, y;
-if (n >= 0 && n <= 15)
-for (i = 0; i <= n; i++)
-{
-_putchar('0');
-for (y = 1; y <= n; y++)
-{
-_putchar(',');
-_putchar(' ');
-x = i * y;
-if (x <= 9)
-_putchar(' ');
-else
-_putchar((x / 10) + '0');
-_putchar((x % 10) + '0');
-}
-_putchar('\n');
-}
+	int i, x, y;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('0');
+			for (y = 1; y <= n; y++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				x = i * y;
+				if (x < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(x + '0');
+				}
+				else if (x < 100)
+				{
+					_putchar(' ');
+					_putchar((x / 10) + '0');
+					_putchar((x % 10) + '0');
+				}
+				else
+				{
+					_putchar((x / 100) + '0');
+					_putchar(((x / 10) % 10) + '0');
+					_putchar((x % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
