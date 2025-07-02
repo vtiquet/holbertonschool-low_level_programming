33894b902632884c49ca8a36e28f2ed097117a10
vtiquet <valentin.tiquet@gmail.com>
2025-07-02 08:10:50 +0000

    Updated 3-mul.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - Entry point of the program
  * @argc: The number of command-line arguments
@@ -7,7 +8,23 @@
  */
 int main(int argc, char *argv[])
 {
-	(void) *argv;
-	printf("%d\n", argc - 1);
-	return (0);
+  int count, n1, n2 = 1;
+
+  if (argc > 1)
+    {
+      for (count = 1; count < argc; count++)
+	{
+		n1 = atoi(argv[count]);
+		n2 = n2 * n1;
+	}
+
+		printf("%d\n", n2);
+    }
+  else
+    {
+      printf("Error\n");
+	  return (1);
+    }
+
+  return (0);
 }
