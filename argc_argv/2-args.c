85e1a52f68c7addbdcd9e18e63f1d5b8a665b192
vtiquet <valentin.tiquet@gmail.com>
2025-07-02 07:53:42 +0000

    Updated 2-args.c

---
+++ argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the command-line arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int count;
+
+    for (count = 0; count < argc; count++)
+	{
+		printf("%s\n", argv[count]);
+	}
+	return (0);
+}
---
+++ argc_argv/3-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the command-line arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void) *argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
