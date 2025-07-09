6cb916906948d2a8cfc6532a90cde170bfd9949f
vtiquet <valentin.tiquet@gmail.com>
2025-07-09 14:03:03 +0200

    Updated 4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -42,17 +42,33 @@ char *_strcpy(char *dest, char *src)
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int i;
+	dog_t *doggo;
+	new_dog = malloc(sizeof(dog_t));
 
 	if (new_dog == NULL)
 	{
-		return;
+		return(NULL);
 	}
-
-
-	if (a == NULL)
+	doggo = malloc(sizeof(dog_t))
+	if (doggo == NULL)
 	{
 		return (NULL);
 	}
-
+	(*doggo).name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if ((*doggo).name == NULL)
+	{
+		free(doggo);
+		return (NULL);
+	}
+	(*doggo).owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if ((*doggo).owner == NULL)
+	{
+		free((*doggo).name);
+		free(doggo);
+		return (NULL);
+	}
+	(*doggo).name = _strcopy((*doggo).name, name);
+	(*doggo).age = age;
+	(*doggo).owner = _strcopy((*doggo).owner, owner);
+	return (doggo);
 }
