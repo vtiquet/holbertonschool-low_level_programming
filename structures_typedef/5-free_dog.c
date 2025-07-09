d5be82d0b5ab5afed8c627c8805fed724de010ba
vtiquet <valentin.tiquet@gmail.com>
2025-07-09 13:37:06 +0000

    Created 5-free_dog.c

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+*free_dog - a function that frees dogs.
+*@d: pointer with dog informations
+.*/
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	if (d->name != NULL)
+	{
+		free(d->name);
+	}
+	if (d->owner != NULL)
+	{
+		free(d->owner);
+		free(d);
+	}
+}
