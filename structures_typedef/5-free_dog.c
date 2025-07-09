37fbe9703df1cfcfb872cfb7341a8c92416ca040
vtiquet <valentin.tiquet@gmail.com>
2025-07-09 13:42:22 +0000

    Updated file

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,9 +1,9 @@
 #include <stdlib.h>
 #include "dog.h"
 /**
-*free_dog - a function that frees dogs.
-*@d: pointer with dog informations
-.*/
+ *free_dog - a function that frees dogs.
+ *@d: pointer with dog informations
+ */
 void free_dog(dog_t *d)
 {
 	if (d == NULL)
