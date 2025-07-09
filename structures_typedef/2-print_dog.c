a96a46c764fc07c62475e1c063381671badc0fde
vtiquet <valentin.tiquet@gmail.com>
2025-07-09 08:03:52 +0000

    Created and Updated files

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -4,9 +4,13 @@
  * print_dog - a function that prints a struct dog
  * @d: the pointer to be printed.
  */
-void print_dog(struct dog *d);
+void print_dog(struct dog *d)
 {
-    (*d).name = name;
-    (*d).age = age;
-    (*d).owner = owner;
+	if (d == NULL)
+	{
+		return;
+	}
+	printf("Name: %s\n", ((*d).name != NULL) ? (*d).name : "(nil)");
+	printf("Age: %f\n", (*d).age);
+	printf("Owner: %s\n", ((*d).owner != NULL) ? (*d).owner : "(nil)");
 }
---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+* _strlen - a function that returns the length of a string.
+* @s: The string to get the legth of.
+* Return: the length of the string.
+*/
+int _strlen(char *s)
+{
+	int n = 0;
+
+	while (*s != '\0')
+	{
+	n++;
+	s++;
+	}
+	return (n);
+}
+/**
+* _strcpy - function that copies the string to the buffer.
+* @dest: the buffer.
+* @src: the string.
+* Return: the pointer to dest.
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int  i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/**
+ * new_dog - a function that creates a new dog.
+ * @name: The dog's name.
+ * @age: The dog's age.
+ * @owner: The owner's name.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if (new_dog == NULL)
+	{
+		return;
+	}
+
+
+	printf("Name: %s\n", ((*d).name != NULL) ? (*d).name : "(nil)");
+	printf("Age: %f\n", (*d).age);
+	printf("Owner: %s\n", ((*d).owner != NULL) ? (*d).owner : "(nil)");
+}
