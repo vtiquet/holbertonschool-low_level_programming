4ff358cc8ef1d83835bbf9f14690d52029528270
vtiquet <valentin.tiquet@gmail.com>
2025-07-28 07:55:19 +0000

    Created Directory and files

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * print_list - a function that prints all the elements of a list.
+ * @n: the number of parameters.
+ * @...: the numbers of parameters stored.
+ * Return: the sum of all its parameters, 0 if n == 0.
+ */
+size_t print_list(const list_t *h)
+{
+
+}
