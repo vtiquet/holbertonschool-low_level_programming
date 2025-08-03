de15364f0e7dc87ca64945c67b06afdd20c7dd27
vtiquet <valentin.tiquet@gmail.com>
2025-08-03 08:46:31 +0000

    Updated to comply with Beety style coding

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -3,7 +3,8 @@
  * hash_table_get - a function that retrieves a value associated with a key.
  * @ht: is the hash table you want to look into.
  * @key: is the key you are looking for.
- * Return: the value associated with the element, or NULL if key couldn’t be found.
+ * Return: the value associated with the element,
+ * or NULL if key couldn’t be found.
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
