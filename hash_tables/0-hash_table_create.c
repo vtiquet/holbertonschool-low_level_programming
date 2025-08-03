ecc30f7075410577e27575bdeed8b2ddc4d660ea
vtiquet <valentin.tiquet@gmail.com>
2025-08-03 10:51:42 +0200

    Revert to older stable version

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -7,21 +7,19 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned int i = 0;
-	hash_table_t *ht = malloc(sizeof(hash_table_t));
+	hash_table_t *ht;
 
+	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
 		return (NULL);
-	}
-	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
+	(*ht).size = size;
+	(*ht).array = malloc(sizeof(hash_node_t *) * size);
+	if ((*ht).array == NULL)
 		return (NULL);
+	while (i < size)
+	{
+		(*ht).array[i] = NULL;
+		i++;
 	}
-	for (; i < size; i++)
-		ht->array[i] = NULL;
 	return (ht);
 }
